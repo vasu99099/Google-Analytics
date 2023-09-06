38a974b515e4853f8461eef29506238b776d03b6
vasu99099 <vasubhimani99099@gmail.com>
2023-09-06 16:26:37 +0530

    analytics google add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-
+import { useEffect } from "react";
+import "./App.css";
+import ReactGA from "react-ga4";
 function App() {
+  useEffect(() => {
+    ReactGA.initialize([
+      {
+        trackingId: " G-X3EGZDWRZR",
+      },
+    ]);
+    ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
+  }, []);
+
+  // Send pageview with a custom path
+
   return (
     <div className="App">
-    <h1>I AM VASU BHIMANI</h1>
+      <h1>I AM VASU BHIMANI</h1>
     </div>
   );
 }
