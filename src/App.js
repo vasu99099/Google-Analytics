9fc5400bd7158804f70761bf3d3e4495943b731b
vasu99099 <99337841+vasu99099@users.noreply.github.com>
2023-10-03 14:18:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./App.css";
 import ReactGA from "react-ga4";
+import OneSignal from 'react-onesignal';
 function App() {
   useEffect(() => {
     ReactGA.initialize([
@@ -9,7 +10,11 @@ function App() {
       },
     ]);
     ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
-    console.log("analysics add")
+    console.log("analysics add");
+    OneSignal.init({ appId: '5ebbed67-be95-4630-b63e-7ec37f467d39' }).then(() => {
+      OneSignal.Slidedown.promptPush();
+      // do other stuff
+    })
   }, []);
 
   // Send pageview with a custom path
